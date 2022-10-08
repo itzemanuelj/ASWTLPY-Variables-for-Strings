a91c51405eeb2093537b49c9fd1701462ef2fef7
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-08 12:51:57 -0400

    mvp commit for variables for strings

---
+++ index.py
@@ -0,0 +1,37 @@
+# Because of the quotation marks, Python identifies the greeting below as a _______ (1 word, lowercase)
+"Hello, World!"
+# string
+
+# Because there are no quotation marks, Python identifies the set of characters below as a _______.
+from tkinter import Variable
+
+# Because there are no quotation marks, Python identifies the set of characters below as a _______.
+# country_of_origin
+country_of_origin = 'USA'
+# Variable
+
+# In the orange box type the next character. I'll autocomplete. Don't type spaces or carriage returns.
+# Assign the string "Boo!" to the variable scare.
+scare = "Boo!"
+
+# Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces. When the exercise is complete, I'll turn the exercise number green.
+# Assign the string "jay" to the variable bird.
+bird = "jay"
+
+# Assign your first name to a variable. You choose the variable name.
+name = "Justin"
+
+# Assign a string to a variable. You choose the variable name and the string.
+city_of_origin = "Detroit"
+
+# Assign a string to a variable. You choose the variable name and the string. Using the variable, write a statement that displays the string on the screen.
+favorite_food = "pizza"
+print(favorite_food)
+
+# Revise this variable name as the Python governing body recommends.
+# countryoforigin
+country_of_origin
+
+# Assign the string "Hi" to the variable greeting
+greeting = "Hi"
+print(greeting)
